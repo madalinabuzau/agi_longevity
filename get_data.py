fc2ab1e64967bf05dd844153b8b4965943ed0a43
madalinabuzau <madalina.buzau@gmail.com>
2024-04-27 19:19:58 -0700

    First commit

---
+++ get_data.py
@@ -0,0 +1,5 @@
+import paperscraper
+
+papers = paperscraper.search_papers('klotho',
+                                    limit=10000000000,
+                                    pdir='klotho_papers')
